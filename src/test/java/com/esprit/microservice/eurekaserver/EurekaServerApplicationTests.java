02841a488764fcc6d2a4fd4afde46a1213ad9a52
loujainrouached <loujein.roauched@esprit.tn>
2026-02-23 10:40:09 +0100

    Initial commit Eureka Server

---
+++ src/test/java/com/esprit/microservice/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.esprit.microservice.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
